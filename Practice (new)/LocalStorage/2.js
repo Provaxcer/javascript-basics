a8fc9a82b06a1623e4e371749acde02186f98c3a
Provaxcer <basaksantanu659@gmail.com>
2026-04-16 19:51:45 +0530

    LocalStorage

---
+++ Practice (new)/LocalStorage/2.js
@@ -0,0 +1,15 @@
+// Save
+localStorage.setItem("username", "Santanu");
+
+// Read
+const name = localStorage.getItem("username");
+console.log(name); // "Santanu"
+
+// Delete
+localStorage.removeItem("username");
+
+// Now this returns null
+console.log(localStorage.getItem("username")); // null
+
+
+//  Saving and loading a name
---
+++ Practice (new)/LocalStorage/3.js
@@ -0,0 +1,11 @@
+// Read existing count, or start at 0
+let count = localStorage.getItem("count");
+count = count ? parseInt(count) : 0;
+
+count++;
+localStorage.setItem("count", count);
+
+console.log("Count:", count);
+
+
+// Persisting a counter across refresh
