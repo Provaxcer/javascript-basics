fed94d53d13019db52f151591b684f0898f2d749
Provaxcer <basaksantanu659@gmail.com>
2026-04-20 16:08:47 +0530

    task

---
+++ Practice (new)/Final Project/index.js
@@ -0,0 +1,40 @@
+const searchBox = document.querySelector("#search-input")
+const title = document.querySelector("#note-title")
+const Textarea = document.querySelector("#note-content")
+const saveBtn = document.querySelector("button")
+const notesCon = document.querySelector("#notes-container")
+
+function loadNotes() {
+    let notes;
+
+    const writtenNotes = localStorage.getItem("notes");
+    if (writtenNotes) {
+        notes = JSON.parse(writtenNotes);
+    }
+    else {
+        notes = [];
+    }
+    console.log(notes)
+}
+
+loadNotes()
+
+saveBtn.addEventListener("click", () => {
+    const noteTitle = title.value;
+    const noteContent = Textarea.value;
+
+    if (!noteTitle || !noteContent) {
+        alert("Please fill in both fields");
+        return;
+    }
+})
+
+const note = {
+    title: "",
+    content: ""
+}
+
+const LoadNotes = () => {
+    const writtenNotes = localStorage.getItem("notes");
+    note.title.push(writtenNotes)
+}
