db56d815f3b5b0273b0fbf25a14cf02ad9ed4927
Provaxcer <basaksantanu659@gmail.com>
2026-04-30 22:10:12 +0530

    node

--- Node/Day1/1.js
+++ Node/Day1/1.js
@@ -1,5 +1,18 @@
 import chalk from 'chalk';
+import colors from 'yoctocolors';
+const log = console.log;
+log(chalk.red('Hello world!'));
+log(chalk.red('Hello world!'));
+log(chalk.bgYellow('Hello world!'));
 
-console.log(chalk.red('Hello world!'));
-console.log(chalk.red('Hello world!'));
-console.log(chalk.bgYellow('Hello world!'));
+
+log(colors.red('Yo!'));
+
+log(colors.blue(`Welcome to the ${colors.green('yoctocolors')} package!`));
+
+
+log(chalk.green(    
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
